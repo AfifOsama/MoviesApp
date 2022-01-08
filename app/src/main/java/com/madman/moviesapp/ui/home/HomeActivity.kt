df8eebd032faae9b9df9aead68fe7c954372c1d4
AfifOsama <afifosama66@gmail.com>
2022-01-08 14:33:28 +0700

    adding tab view at home

--- app/src/main/java/com/madman/moviesapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/madman/moviesapp/ui/home/HomeActivity.kt
@@ -1,12 +1,20 @@
 package com.madman.moviesapp.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.madman.moviesapp.R
+import androidx.appcompat.app.AppCompatActivity
+import com.madman.moviesapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        with(binding) {
+            viewPager.adapter = sectionsPagerAdapter
+            tabs.setupWithViewPager(viewPager)
+        }
+        supportActionBar?.elevation = 0f
     }
 }
